264b00565a0ea8fefe7b2c941c557a7a5ddcdf6c
yaegi <keavonia@gmail.com>
2025-08-12 18:44:49 +0200

    add code

---
+++ PyTest - parametrize/test_items.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def test_basket(browser):
+
+    browser.get(link)
+
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn.btn-add-to-basket")
+
+    assert button.is_displayed(), "Кнопка добавления в корзину не найдена или не отображается"
+
+    time.sleep(10)
